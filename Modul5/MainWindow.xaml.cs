d3688065cccdb72ecc0fda3e9ca5ae747363ad83
Арсений Бодунов <arseniyex@gmail.com>
2023-10-10 10:54:38 +0300

    Добавьте файлы проекта.

---
+++ Modul5/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Modul5
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private double originalImageWidth;
+        private double originalImageHeight;
+
+        // Обработчик события для выбора изображения
+        private void SelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Изображения (*.png, *.jpg)|*.png;*.jpg|Все файлы (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string imagePath = openFileDialog.FileName;
+
+                // Загрузка выбранного изображения и отображение его на элементе ImageControl
+                BitmapImage bitmap = new BitmapImage(new Uri(imagePath));
+                ImageControl.Source = bitmap;
+
+                // Сохранение исходных размеров изображения
+                originalImageWidth = bitmap.PixelWidth;
+                originalImageHeight = bitmap.PixelHeight;
+            }
+        }
+
+        private void ZoomSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double zoomFactor = ZoomSlider.Value;
+
+            // Установка нового масштаба изображения
+            ScaleTransform scaleTransform = new ScaleTransform(zoomFactor, zoomFactor);
+            ImageControl.LayoutTransform = scaleTransform;
+        }
+    }
+}
